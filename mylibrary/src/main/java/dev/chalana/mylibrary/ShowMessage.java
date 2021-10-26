5702ef7ddd5edc3df58503faa6583ae7982694c6
Chalana <chalanamg@gmail.com>
2021-10-27 00:18:31 +0530

    initial

---
+++ mylibrary/src/main/java/dev/chalana/mylibrary/ShowMessage.java
@@ -0,0 +1,10 @@
+package dev.chalana.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ShowMessage {
+    public void show(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
