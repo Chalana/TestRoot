5270d2f3b7ce98b6672aee1eb5c7de09593059f6
Chalana <chalanamg@gmail.com>
2021-10-27 12:14:31 +0530

    1.8

--- mylibrary/src/main/java/dev/chalana/mylibrary/ShowMessage.java
+++ mylibrary/src/main/java/dev/chalana/mylibrary/ShowMessage.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class ShowMessage {
     public void show(Context context, String message){
-        Toast.makeText(context, "msg::"+message, Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "1.8"+message, Toast.LENGTH_LONG).show();
 
     }
 }
